14342cc67752c63986c4b5d43b5e3284ca8c1b5b
Feilan Jiang <f-jiang@github.com>
2016-11-06 17:41:23 -0500

    Changes: - remove struct hack code from |snake_init()| - implement bodies of snake blackbox functions - make parameters const pointers to |Snake| - remove function |snake_eat()| - add |last_x|, |last_y|, and |dir| to |struct snake_t|

--- src/gameobj.c
+++ src/gameobj.c
@@ -2,17 +2,31 @@
 
 #include <stdlib.h>
 
+// C struct "hack"
+// if realloc'ing s->x and s->y, would have to realloc entire struct as well
+// Snake *s = malloc(sizeof(*s) + 2 * sizeof(int));
+// s->len = 1;
+// 
+// s->x = (int *) (s + 1);
+// s->y = (int *) (s + 1) + 1;
 Snake *snake_init(int x, int y) {
-    Snake *s = malloc(sizeof(*s) + 2 * sizeof(int));
-    s->len = 1;
+    Snake *s = malloc(sizeof(*s));
 
-    s->x = (int *) (s + 1);
-    s->y = (int *) (s + 1) + 1;
+    s->len = 1;
+    s->x = malloc(s->len * sizeof(*(s->x))); 
+    s->y = malloc(s->len * sizeof(*(s->y))); 
+
     *(s->x) = x;
     *(s->y) = y;
 
     s->head_x = s->x;
     s->head_y = s->y;
+
+    s->last_x = *(s->x);
+    s->last_y = *(s->y);
+
+    s->dir = NONE;
+    
     return s;
 }
 
@@ -23,15 +37,58 @@ Food *food_init(int x, int y) {
     return f;
 }
 
-static void snake_grow(Snake *s) {
+// can only grow once per movement
+void snake_grow(Snake * const s) {
+    if (*(s->x + s->len - 1) != s->last_x || *(s->y + s->len - 1) != s->last_y) {
+        s->len++;
+        int *x1 = realloc(s->x, s->len * sizeof(*x1)); 
+        int *y1 = realloc(s->y, s->len * sizeof(*y1)); 
 
+        if (x1 && y1) {
+            s->head_x = s->x = x1;
+            s->head_y = s->y = y1;
+            *(s->x + s->len - 1) = s->last_x;
+            *(s->y + s->len - 1) = s->last_y;
+        } else {
+            free(x1);
+            free(y1);
+        }
+    }
 }
 
-void snake_eat(Snake *s) {
+void snake_move(Snake * const s, Direction d) {
+    if (~d != s->dir) { // snake can't move "backward"
+        int x = *(s->head_x);
+        int y = *(s->head_y);
 
-}
+        switch (d) {
+            case LEFT:
+                x -= 1;
+                break;
+            case RIGHT:
+                x += 1;
+                break;
+            case UP:
+                y += 1;
+                break;
+            case DOWN:
+                y -= 1;
+                break;
+            default:
+                return;
+        }
 
-void snake_move(Snake *s, Direction d) {
+        int i = s->len - 1;
+        s->last_x = *(s->x + i);
+        s->last_y = *(s->y + i);
+        for ( ; i > 0; i--) {
+            *(s->x + i) = *(s->x + i - 1);
+            *(s->y + i) = *(s->y + i - 1);
+        }
+        *(s->x) = x;
+        *(s->y) = y;
 
+        s->dir = d;
+    }
 }
 
--- src/gameobj.h
+++ src/gameobj.h
@@ -3,7 +3,10 @@
 
 #include <stddef.h>
 
-typedef enum direction_t { LEFT, RIGHT, UP, DOWN } Direction;
+// 00, 01, 10, 11, 100
+// ~UP == DOWN
+// ~LEFT == RIGHT
+typedef enum direction_t { UP = 0, LEFT, RIGHT, DOWN, NONE } Direction;
 
 // struct should be defined in .h not in .c
 // so other .c files can "see" the struct def
@@ -11,6 +14,8 @@ typedef struct snake_t {
     size_t len;
     int *x, *y;
     int *head_x, *head_y;
+    int last_x, last_y;
+    Direction dir;
 } Snake;
 
 typedef struct food_t {
@@ -20,7 +25,7 @@ typedef struct food_t {
 Snake *snake_init(int x, int y);
 Food *food_init(int x, int y);
 
-void snake_eat(Snake *s);
-void snake_move(Snake *s, Direction d);
+void snake_grow(Snake * const s);
+void snake_move(Snake * const s, Direction d);
 
 #endif
